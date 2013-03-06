325784fc897b7baf414f0000e351bd7f897a4914
Bernie <berniebogart@Bernies-MacBook-Air.local>
2013-03-06 11:50:47 +0100

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Bernie on 06.03.13.
+//  Copyright (c) 2013 bfk engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Bernie on 06.03.13.
+//  Copyright (c) 2013 bfk engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
